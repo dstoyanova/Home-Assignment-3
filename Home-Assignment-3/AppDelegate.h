b5488f47a2b4c0bc7873fa5c68ebde82904a79ab
Desislava Stoyanova <stoyanova@Desislavas-MacBook-Air.local>
2015-04-23 14:40:40 +0300

    Initial Commit

---
+++ Home-Assignment-3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Home-Assignment-3
+//
+//  Created by Desislava Stoyanova on 4/23/15.
+//  Copyright (c) 2015 Desislava Stoyanova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Home-Assignment-3/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Home-Assignment-3
+//
+//  Created by Desislava Stoyanova on 4/23/15.
+//  Copyright (c) 2015 Desislava Stoyanova. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
